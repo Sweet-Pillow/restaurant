cc541f36e65ab3cde8c058d05a6536343b2a6f4b
Daniel Alves Farias <daniel_h.m@outlook.com>
2023-01-25 17:08:41 -0300

    Config modules

---
+++ src/pages/Menu/index.tsx
@@ -0,0 +1,11 @@
+import Styles from './Menu.module.scss';
+
+export default function Menu(){
+    return(
+        <main>
+            <nav className="menu">
+
+            </nav>
+        </main>
+    )
+}
